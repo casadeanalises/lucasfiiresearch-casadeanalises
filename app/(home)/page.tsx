be04eef78fc5bb18fe487b8fd9cc5e97bc5db773
Alan Rocha <alanrochaarg2001@gmail.com>
2025-04-09 11:11:53 -0300

    feat(page): forçar renderização dinâmica e desativar cache na página inicial
    - Adicionadas as exportações `dynamic`, `fetchCache` e `revalidate` para garantir que a renderização da página inicial seja sempre dinâmica e sem cache, melhorando a atualização de conteúdo.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
+
 import { useEffect } from "react";
 import Image from "next/image";
 import { Button, buttonVariants } from "../_components/ui/button";
