85151463e1cbb7321dcca8070490c6b7295fb027
Alan Rocha <alanrochaarg2001@gmail.com>
2025-04-09 09:38:22 -0300

    feat(page): implementar renderização dinâmica com carregamento assíncrono
    - Adicionada a funcionalidade de renderização dinâmica à página inicial utilizando `next/dynamic` e `Suspense` para melhorar a experiência do usuário durante o carregamento.
    - Implementado um componente de carregamento visual enquanto a página é carregada, garantindo uma interface mais responsiva.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
-import Page from "./(home)/page";
+"use client";
+
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+const HomePage = dynamic(() => import("./(home)/page"), {
+  ssr: true,
+  loading: () => (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-gray-900"></div>
+    </div>
+  ),
+});
 
 export default function Home() {
-  return <Page />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex min-h-screen items-center justify-center">
+          <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-gray-900"></div>
+        </div>
+      }
+    >
+      <HomePage />
+    </Suspense>
+  );
 }
